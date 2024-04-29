2a927783f2ece7bb57a142ce8b43c24638b35bad
Ahammad Nafiz <ahammadnafiz@outlook.com>
2024-04-30 00:49:34 +0600

    update code editor

--- codeditor.py
+++ codeditor.py
@@ -15,7 +15,6 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 class PredictaCodeEditor:
     def __init__(self):
         self.load_buttons()
-        self.load_info_bar()
         self.load_code_editor_settings()
 
     def load_buttons(self):
@@ -23,9 +22,7 @@ class PredictaCodeEditor:
             {"name": "copy", "feather": "Copy", "hasText": True, "alwaysOn": True, "commands": ["copyAll"], "style": {"top": "0rem", "right": "0.4rem"}},
             {"name": "run", "feather": "Play", "primary": True, "hasText": True, "showWithIcon": True, "commands": ["submit"], "style": {"bottom": "0rem", "right": "0.4rem"}}
         ]
-    
-    def load_info_bar(self):
-        self.info_bar = {}
+
 
     def load_code_editor_settings(self):
         self.demo_sample_python_code = '''# Start Writing Code!'''
@@ -67,7 +64,7 @@ class PredictaCodeEditor:
 
         st.markdown('<div class="centered">', unsafe_allow_html=True)
        
-        response_dict = code_editor(self.demo_sample_python_code, height=height, lang=language, buttons=self.custom_buttons_alt, info=self.info_bar)
+        response_dict = code_editor(self.demo_sample_python_code, height=height, lang=language, buttons=self.custom_buttons_alt)
 
         if response_dict['type'] == "submit" and len(response_dict['text']) != 0:
             st.write('Output')
