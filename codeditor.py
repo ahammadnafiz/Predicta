5690a85407def71c6e7b617e08704c3cdea2ce44
ahammadnafiz <ahammadnafiz@outlook.com>
2024-05-11 23:18:57 +0600

    update repo

--- codeditor.py
+++ codeditor.py
@@ -12,29 +12,44 @@ from scipy.stats import zscore
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 class PredictaCodeEditor:
+    """
+    A class to create a Predicta Code Editor in a Streamlit app.
+    """
+
     def __init__(self):
+        """
+        Initialize the class and load the custom buttons and code editor settings.
+        """
         self.load_buttons()
         self.load_code_editor_settings()
 
     def load_buttons(self):
-        self.custom_buttons_alt = [
+        """
+        Define custom buttons for the code editor.
+        """
+        self.custom_buttons = [
             {"name": "copy", "feather": "Copy", "hasText": True, "alwaysOn": True, "commands": ["copyAll"], "style": {"top": "0rem", "right": "0.4rem"}},
             {"name": "run", "feather": "Play", "primary": True, "hasText": True, "showWithIcon": True, "commands": ["submit"], "style": {"bottom": "0rem", "right": "0.4rem"}}
         ]
 
-
     def load_code_editor_settings(self):
+        """
+        Define the initial code and mode for the code editor.
+        """
         self.demo_sample_python_code = '''# Start Writing Code!'''
         self.mode_list = ["python"]
 
     def run_code_editor(self, df):
-        
+        """
+        Run the Predicta Code Editor in a Streamlit app.
+
+        Args:
+            df (pandas.DataFrame): The dataset to be displayed.
+        """
         st.markdown("<h2 style='text-align: center; font-size: 20px;'>Dataset</h2>", unsafe_allow_html=True)
         st.dataframe(df, width=800)
-        
         height = 600
         language = "python"
-        
         st.markdown("<div style='text-align: center; margin-top: 20px; margin-bottom: 20px; font-size: 30px;'>Predicta Code Editor.</div>", unsafe_allow_html=True)
 
         with st.sidebar:
@@ -62,8 +77,7 @@ class PredictaCodeEditor:
         )
 
         st.markdown('<div class="centered">', unsafe_allow_html=True)
-       
-        response_dict = code_editor(self.demo_sample_python_code, height=height, lang=language, buttons=self.custom_buttons_alt)
+        response_dict = code_editor(self.demo_sample_python_code, height=height, lang=language, buttons=self.custom_buttons)
 
         if response_dict['type'] == "submit" and len(response_dict['text']) != 0:
             st.write('Output')
@@ -81,4 +95,5 @@ class PredictaCodeEditor:
             if output:
                 st.code(output, language=response_dict['lang'])
             buffer.close()
+
         st.markdown('</div>', unsafe_allow_html=True)
