7ee234ef38366dd848d4a09f1e0c5a5eb02ae12d
ahammadnafiz <ahammadnafiz@outlook.com>
2025-01-19 14:39:25 +0600

    Refactor encoding methods to use category_encoders library and update requirements.txt to include category_encoders dependency

--- FeatureEngineering/encoding.py
+++ FeatureEngineering/encoding.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 import streamlit as st
 import category_encoders as ce
 from show_code import ShowCode
@@ -13,14 +12,14 @@ class DataEncoder:
         self.view_code.set_target_class(DataEncoder)
 
     def label_encoding(self, col):
-        label_encoder = LabelEncoder()
-        self.data[col] = label_encoder.fit_transform(self.data[col])
+        encoder = ce.OrdinalEncoder(cols=[col])
+        self.data[col] = encoder.fit_transform(self.data[col])
         return self.data
 
     def one_hot_encoding(self, col):
-        if len(self.data[col].unique()) > 2:  
-            encoder = OneHotEncoder(sparse_output=False, drop='first')
-            encoded_values = encoder.fit_transform(self.data[[col]])
+        if len(self.data[col].unique()) > 2:
+            encoder = ce.OneHotEncoder(cols=[col], use_cat_names=True)
+            encoded_values = encoder.fit_transform(self.data)[encoder.feature_names_]
             new_cols = [col + '_' + str(i) for i in range(encoded_values.shape[1])]
             encoded_df = pd.DataFrame(encoded_values, columns=new_cols, index=self.data.index)
             self.data = pd.concat([self.data, encoded_df], axis=1)
@@ -35,8 +34,8 @@ class DataEncoder:
 
     def target_encoding(self, col, target_col):
         if len(self.data[col].unique()) > 2:
-            target_encoder = ce.TargetEncoder(cols=[col])
-            self.data[col + '_target_encoded'] = target_encoder.fit_transform(self.data[col], self.data[target_col])
+            encoder = ce.TargetEncoder(cols=[col])
+            self.data[col] = encoder.fit_transform(self.data[col], self.data[target_col])
         return self.data
 
     def frequency_encoding(self, col):
@@ -45,14 +44,14 @@ class DataEncoder:
         return self.data
 
     def binary_encoding(self, col):
-        binary_encoder = ce.BinaryEncoder(cols=[col])
-        self.data = binary_encoder.fit_transform(self.data)
+        encoder = ce.BinaryEncoder(cols=[col])
+        self.data = encoder.fit_transform(self.data)
         return self.data
 
     def encoder(self):
         st.markdown("<h1 style='text-align: center; font-size: 30px;'>Encode Data</h1>", unsafe_allow_html=True)
         st.markdown("---")
-        st.markdown("<h2 style='text-align: center; font-size: 20px;'>Dataset</h1>", unsafe_allow_html=True)
+        st.markdown("<h2 style='text-align: center; font-size: 20px;'>Dataset</h2>", unsafe_allow_html=True)
         st.dataframe(self.data, width=800)
 
         options = [
@@ -67,7 +66,7 @@ class DataEncoder:
         option = st.sidebar.selectbox("Select an Encoding Method", options)
 
         column_to_encode = st.sidebar.selectbox("Select column for Encoding", self.data.columns)
-        
+
         if option in ["Mean Encoding", "Target Encoding"]:
             target_col = st.sidebar.selectbox("Select target column", self.data.columns)
         else:
@@ -101,7 +100,7 @@ class DataEncoder:
             st.dataframe(st.session_state.encoded_data)
 
             st.session_state.show_code = st.checkbox('Show Code', value=st.session_state.show_code)
-            
+
             if st.session_state.show_code:
                 if option == "Label Encoding":
                     self.view_code._display_code('label_encoding')
